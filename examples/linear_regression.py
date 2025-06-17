6b9b25439ee1463ef4100196c3c28a31edf479e3
nk-hystax <nkovaleva@hystax.com>
2025-06-17 07:02:25 +0000

    KF-95. Added arcee code

---
+++ examples/linear_regression.py
@@ -0,0 +1,125 @@
+from __future__ import absolute_import, division, print_function
+
+import tensorflow as tf
+import numpy as np
+
+import kiroframe_arcee as arcee
+
+STATS_FILENAME = 'stats.csv'
+
+# init arcee
+arcee.init(token="test", task_key="linear_regression")
+
+arcee.model("linear_regression")
+
+arcee.tag("project", "regression")
+
+arcee.milestone("preparing data")
+
+rng = np.random
+
+learning_rate = 0.01
+training_steps = 2000
+display_step = 50
+
+X = np.array(
+    [
+        3.3,
+        4.4,
+        5.5,
+        6.71,
+        6.93,
+        4.168,
+        9.779,
+        6.182,
+        7.59,
+        2.167,
+        7.042,
+        10.791,
+        5.313,
+        7.997,
+        5.654,
+        9.27,
+        3.1,
+    ]
+)
+Y = np.array(
+    [
+        1.7,
+        2.76,
+        2.09,
+        3.19,
+        1.694,
+        1.573,
+        3.366,
+        2.596,
+        2.53,
+        1.221,
+        2.827,
+        3.465,
+        1.65,
+        2.904,
+        2.42,
+        2.94,
+        1.3,
+    ]
+)
+
+
+W = tf.Variable(rng.randn(), name="weight")
+b = tf.Variable(rng.randn(), name="bias")
+
+
+def linear_regression(x):
+    return W * x + b
+
+
+def mean_square(y_pred, y_true):
+    return tf.reduce_mean(tf.square(y_pred - y_true))
+
+
+optimizer = tf.optimizers.SGD(learning_rate)
+
+
+def run_optimization():
+    with tf.GradientTape() as g:
+        pred = linear_regression(X)
+        loss = mean_square(pred, Y)
+    gradients = g.gradient(loss, [W, b])
+    optimizer.apply_gradients(zip(gradients, [W, b]))
+
+
+arcee.milestone("calculation")
+stats = []
+for step in range(1, training_steps + 1):
+    run_optimization()
+
+    if step % display_step == 0:
+        pred = linear_regression(X)
+        loss = mean_square(pred, Y)
+        # send model stats
+        arcee.send({"step": step, "loss": float(loss)})
+        print(
+            "step: %i, loss: %f, W: %f, b: %f"
+            % (step, loss, W.numpy(), b.numpy())
+        )
+        stats.append(",".join((str(x) for x in [step, float(loss),
+                                                float(W.numpy()),
+                                                float(b.numpy())])))
+
+arcee.milestone("calc done")
+arcee.model_version_alias("champion")
+
+try:
+    with open(STATS_FILENAME, 'w') as file:
+        for line in stats:
+            file.write(line + '\n')
+        arcee.artifact(
+            STATS_FILENAME,
+            'Run stats',
+            tags={'training_steps': training_steps}
+        )
+    arcee.finish()
+except Exception as exc:
+    print("Error: %s" % str(exc))
+    arcee.error()
---
+++ examples/simple.py
@@ -0,0 +1,19 @@
+import kiroframe_arcee as arcee
+
+with arcee.init("test", "simple"):
+    arcee.tag("key", "value")
+    arcee.tag("test1", "test2")
+    arcee.milestone("just a milestone")
+    arcee.model("model_key", "/src/simple.py")
+    arcee.model_version("1.23.45-rc")
+    arcee.model_version_alias("winner")
+    arcee.model_version_tag("key", "value")
+    arcee.send({"t": 2})
+    arcee.artifact(
+        "https://s3/ml-bucket/artifacts/AccuracyChart.png",
+        "Accuracy line chart",
+        "The dependence of accuracy on the time",
+        {"env": "staging"})
+    arcee.artifact_tag("https://s3/ml-bucket/artifacts/AccuracyChart.png",
+                       "key", "value")
+print(arcee.info())
